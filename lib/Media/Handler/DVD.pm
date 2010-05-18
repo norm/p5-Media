09a15ac75fc82205a36ac5a3d1081bdf3959b912
Mark Norman Francis <norm@cackhanded.net>
2010-05-18 09:07:14 +0100

    Document the DVD description file.

--- lib/Media/Handler/DVD.pm
+++ lib/Media/Handler/DVD.pm
@@ -234,3 +234,131 @@ method edit_dvd_config ( Str $directory ) {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+B<Media::Handler::DVD> - DVD specific handling code
+
+=head1 DVD DESCRIPTION FILE
+
+When first processing a DVD, the C<media> script will examine the DVD and
+write out a F<dvd.conf> file, which is a skeleton description of the contents
+of the DVD.
+
+Like the L<Media::Config> configuration, lines beginning with a hash (#)
+character are comments, which are ignored. The skeleton file will contain
+various comments informing you of what the scan of the DVD found.
+
+=head2 Introduction section
+
+The first part of the file tells C<Media> what type of content is on the DVD,
+television episodes or a movie; what the title is and anything else that is
+disc-wide information.
+
+=head3 Television show example
+
+    type   = TV
+    series = Firefly
+    season = 1
+
+=head3 Movie example
+
+    type   = Movie
+    title  = Serenity
+    year   = 2005
+    poster = http://...
+    rating = 15
+
+=head2 Title sections
+
+An individual video file (an episode of a television series, a movie, extra
+content such as out-takes) on a DVD is called a title.
+
+Each title on the DVD has a section in the description file, which contains
+details about that specific title.
+
+=head3 Television show example
+
+    [5]
+    audio      = 1:ac3:Sound
+    audio      = 1:aac:Sound
+    title      = Our Mrs Reynolds
+    episode    = 5
+
+=head3 Movie feature example
+
+    [5]
+    audio      = 1:ac3:Surround Sound
+    audio      = 1:aac:Surround Sound
+    audio      = 2:aac:Director's Commentary
+    subtitle   = english.srt
+    subtitle   = commentary.srt
+    feature    = 1
+
+=head3 Movie extra content example
+
+    [5]
+    audio      = 1:aac:Sound
+    extra      = Out-takes
+
+=head2 Available options
+
+More options are available in the per-title sections than
+are shown in the example.
+
+=over
+
+=item B<ignore>
+
+Set this to C<1> to tell C<Media> not to bother with this title. Useful for
+titles such as adverts, FBI warnings, animated menus and more.
+
+=item B<crop>
+
+Pass a specific crop value to HandBrake when processing the video.
+This is inserted into the file when created for each title, using
+automatic detection. Almost always this value is correct.
+
+=item B<audio>
+
+An audio profile. See L<Audio profiles> in L<Media::Tutorial> for more
+information.
+
+The actual audio found when scanning the DVD is listed in the comments for
+that title. The default is to have two profiles on the first channel, one the
+original AC3 audio, the other a reprocessed version of this which is suitable
+for devices that cannot play AC3.
+
+=item B<subtitle>
+
+A subtitle file, in the SRT format. Currently, the language value of this
+subtitle in the output file will be taken from a hard-coded list. This will
+change soon.
+
+=item B<range>
+
+Set this to a range value (eg. C<1-3>) to only convert some of
+the chapters of the title.
+
+=item B<chapters>
+
+Set this to C<1> to copy the chapter markers from the title to the converted
+video. Set this to the filename of a chapter description file if you wish to
+have named chapters.
+
+=item B<decomb>
+
+Set this to C<1> to turn on HandBrake's default decombing, or set it to
+a value to control the decombing further.
+
+=item B<detelecine>
+
+Set this to C<1> to turn on HandBrake's default detelecine processing, or
+set it to a value to control this behaviour further.
+
+=item B<denoise>
+
+Set this to C<weak>, C<medium>, C<strong> or a more detailed control string
+to turn on HandBrake's denoise filtering.
